4bb5d5bd2b9a5d77abfccfead656425fbec87c67
Kristian Lein-Mathisen <kristian@adellica.com>
2018-05-16 14:51:06 +0200

    tests: adds some more testing

--- tests/run.scm
+++ tests/run.scm
@@ -1,6 +1,33 @@
 (import stb-image (chicken blob) srfi-4
 	(chicken file)
-	(chicken pathname))
+	(chicken pathname)
+	test)
+
+
+(test
+ "read-image 2x2 grayscale png"
+ ;;   pixels       w h c
+ `(#${00 45 ab ff} 2 2 1)
+ (receive
+     (with-input-from-string
+	 (conc "\211PNG\r\n\x1a\n\x00\x00\x00\r"
+	       "IHDR\x00\x00\x00\x02\x00\x00\x00\x02\b\x00"
+	       "\x00\x00\x00W\335R\370\x00\x00\x00\x11"
+	       "IDAT\b\x1d\x01\x06\x00\371\377\x00\x00E\x00"
+	       "\253\377\x03o\x01\xf0\xd7\xb5\x30}\x00\x00\x00\x00"
+	       "IEND\256B`\202")
+       read-image)))
+
+(test
+ "load-image 2x2 grayscale tga"
+ ;;   pixels       w h c
+ `(#${00 45 ab ff} 2 2 1)
+ (receive
+     (load-image (blob->string #${00000b00000000000000000002000200080001
+				  abff0100450000000000000000545255455649
+				  53494f4e2d5846494c452e00}))))
+
+
 
 (define (write-pgm blob w h)
   (let ((v (blob->u8vector/shared blob)))
@@ -65,3 +92,5 @@
 (display "\nExpecting \"unknown image type\" error here: ")
 (handle-exceptions e (print-error-message e)
 		   (with-input-from-string "bad example" read-image))
+
+(test-exit)
